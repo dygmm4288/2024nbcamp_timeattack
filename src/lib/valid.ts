9d86ae61d98a7ed0281f9c935d6a5714a6c8a44f
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-01-06 14:30:29 +0900

    add  : 에러 명세, 유효성 검사 함수 추가

--- src/lib/valid.ts
+++ src/lib/valid.ts
@@ -1,3 +1,22 @@
 export function checkValidEmail(email: string) {
   return email.includes("@");
 }
+export function checkValidNickname(nickname: string) {
+  return nickname.length > 2;
+}
+export function checkValidPassword(password: string) {
+  return password.length > 7;
+}
+export function checkValidPasswordConfirm(
+  password: string,
+  passwordConfirm: string,
+) {
+  return password === passwordConfirm;
+}
+
+export const ERROR_MESSAGE = {
+  EMAIL: "이메일 형식이 올바르지 않습니다.",
+  PASSWORD: "비밀번호는 8자 이상이어야 합니다.",
+  PASSWORD_CONFIRM: "비밀번호가 일치하지 않습니다.",
+  NICKNAME: "닉네임은 2자 이상이어야 합니다.",
+};
