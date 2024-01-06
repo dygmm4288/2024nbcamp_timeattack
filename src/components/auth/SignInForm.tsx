4ab3a35dce09674a7239143de6cc45e35d5a8367
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-01-06 14:46:56 +0900

    update: 에러 핸들링 추가

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { login } from "@/app/api/route";
 import useInput from "@/app/hooks/useInput";
+import { checkValidEmail } from "@/lib/valid";
 import { setAuthState } from "@/modules/auth/authSliceRe";
 import { useAppDispatch } from "@/modules/hooks/hook";
 import { AxiosError } from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { FormEvent } from "react";
+import { ToastContainer, toast } from "react-toastify";
 import Input from "./Input";
 
 export default function SignInForm() {
@@ -18,7 +20,12 @@ export default function SignInForm() {
 
   const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
+    const isValidEmail = checkValidEmail(email.value);
+    if (!isValidEmail) {
+      toast.error("이메일 형식이 올바르지 않습니다.");
+      setEmailError("이메일 형식이 올바르지 않습니다.");
+      return;
+    }
     try {
       const res = await login({ id: email.value, password: password.value });
       if (res.success) {
@@ -38,34 +45,44 @@ export default function SignInForm() {
       // TODO: 에러 처리 해야 됨
       const err = error as AxiosError;
       const { message } = err.response!.data as { message: string };
-
+      toast.error(message);
+      console.log(message);
       if (message === "존재하지 않는 유저입니다.") {
-        return;
+        setEmailError(message);
+      }
+      if (
+        message === "비밀번호가 일치하지 않습니다." ||
+        message === "password는 4글자 이상의 문자열이어야 합니다."
+      ) {
+        setPasswordError(message);
       }
     }
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <Input
-        id='email'
-        label='이메일'
-        placeholder='이메일을 입력해주세요'
-        value={email.value}
-        handleChange={handleChangeEmail}
-        errorMessage={email.error}
-      />
-      <Input
-        id='password'
-        type='password'
-        label='비밀번호'
-        placeholder='비밀번호를 입력해주세요'
-        value={password.value}
-        handleChange={handleChangePassword}
-        errorMessage={password.error}
-      />
-      <Link href='/signup'>비밀번호를 잊으셨나요?</Link>
-      <button type='submit'>로그인</button>
-    </form>
+    <>
+      <form onSubmit={handleLogin}>
+        <Input
+          id='email'
+          label='이메일'
+          placeholder='이메일을 입력해주세요'
+          value={email.value}
+          handleChange={handleChangeEmail}
+          errorMessage={null}
+        />
+        <Input
+          id='password'
+          type='password'
+          label='비밀번호'
+          placeholder='비밀번호를 입력해주세요'
+          value={password.value}
+          handleChange={handleChangePassword}
+          errorMessage={null}
+        />
+        <Link href='/signup'>비밀번호를 잊으셨나요?</Link>
+        <button type='submit'>로그인</button>
+      </form>
+      <ToastContainer />
+    </>
   );
 }
